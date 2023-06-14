bf26e85a529a58de1beb563d6b4ae5df880e1e17
Reginaldo-Viana <reginaldo.dearaujo@hotmail.com>
2023-06-14 20:33:36 -0300

    Melhoria no formulário e  listagem de Clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -16,10 +16,16 @@ export class ClientesService {
     return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
   }
 
-  getCliente():Cliente{
-    let cliente : Cliente = new Cliente();
-    cliente.nome = 'Fulano de tal';
-    cliente.cpf = '8888888888'
-    return cliente;
+  // getClientes() : Observable<Cliente[]>{
+  //   return null;
+  // }
+
+  getClientes() : Cliente[]{
+    let cliente = new Cliente();
+    cliente.id = 1;
+    cliente.nome = 'Fulano';
+    cliente.dataCadastro = '18/04/2023'
+    cliente.cpf = '12345678912'
+    return [cliente]
   }
 }
